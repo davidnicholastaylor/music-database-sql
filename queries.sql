28ff2dc42134cf22b04143ec9e579642b7b87ee1
Andy Collins <andy.collins@relode.com>
2018-10-11 15:39:33 -0500

    added update method

--- queries.sql
+++ queries.sql
@@ -24,3 +24,22 @@ JOIN Artist ar ON ar.ArtistId = s.ArtistId
 JOIN Genre g ON g.GenreId = s.GenreId
 ;
 
+INSERT INTO Artist 
+(ArtistName, YearEstablished)
+values
+("Mogwai", 1993)
+;
+
+SELECT * FROM Artist;
+
+DELETE FROM Artist WHERE ArtistId = 28;
+
+DELETE FROM Artist WHERE ArtistId = 33;
+
+UPDATE Artist 
+SET ArtistName = "Modest Mouse"
+WHERE ArtistId = 29;
+
+UPDATE Artist 
+SET YearEstablished = 2008
+WHERE ArtistId = 31;
